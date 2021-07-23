bd6b6b15e277e6d06deca46a28ee72fa2d6831ab
Matteo Volpe <m.volpe@nexsoft.it>
2021-07-23 09:39:52 +0200

    Aggiunda dell'esempio della classe configuration e metedo annotato con @ bean

---
+++ dispositivoMedico/dispositivoMedico/src/main/java/com/example/DM/dispositivoMedico/configure/ProvaConfig.java
@@ -0,0 +1,26 @@
+package com.example.DM.dispositivoMedico.configure;
+
+import com.example.DM.dispositivoMedico.service.DefaultDispositivoMedicoService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+
+/**
+ * Esempio di classe configuration per creare un bean
+ */
+public class ProvaConfig {
+
+
+   /**@Bean
+    * @Scope("singleton")
+    *
+    public DefaultDispositivoMedicoService prova(){
+
+        return  new DefaultDispositivoMedicoService();
+    }
+**/
+
+
+
+}
